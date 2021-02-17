c513c1a0df9bec6e8f988dfeccdbc22a197bbef3
irvincnt <irvincnt93@gmail.com>
2021-02-17 16:25:40 -0600

    add react-big-calendar and configure the calendarPage component

--- src/components/calendar/CalendarPage.js
+++ src/components/calendar/CalendarPage.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Navbar from '../ui/Navbar';
+import moment from 'moment'
+
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { messages } from '../helpers/calendar.messages';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es-mx'
+
+moment.locale('es-mx')
+const localizer = momentLocalizer(moment)
+
+const event = [{
+  title: 'Cumpleaños de ',
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgcolor: '#fafafa',
+  notes: 'Comprar pastel'
+}]
 
 const CalendarPage = () => {
   return ( 
-    <>
+    <div className="calendar-view">
       <Navbar />
-    </>
+        <Calendar
+          localizer={localizer}
+          events={event}
+          startAccessor="start"
+          endAccessor="end"
+          messages={messages}
+        />
+    </div>
    );
 }
  
